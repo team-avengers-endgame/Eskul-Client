bf6ed575b74f0394023795262d5f5d432968829f
Your Name <you@example.com>
2022-02-08 01:31:04 +0600

    img error solved

--- client/src/Pages/Login/Registration/Registration.js
+++ client/src/Pages/Login/Registration/Registration.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import RegisterImg from '../Login_Img/Register-img.svggi';
+import RegisterImg from '../Login_Img/Register-img.svg';
 const Registration = () => {
     return (
         <div className="container">
