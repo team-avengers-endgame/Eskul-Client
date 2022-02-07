c56c777a905944f182c2884ba28a5b265ebbd3ad
Farhan Elias <faarhaan10@gmail.com>
2022-02-08 00:31:02 +0600

    login test

--- client/src/Pages/Login/Login/Login.js
+++ client/src/Pages/Login/Login/Login.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Login = () => {
     return (
         <div>
-            <h1>loginn</h1>
+            <h1>login will done by rukon bro</h1>
         </div>
     );
 };
