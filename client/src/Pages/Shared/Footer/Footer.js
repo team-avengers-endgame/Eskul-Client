39a08612b7432442e3234f60608f324ff120f4af
Taniatabassumnoor <taniatabassumnoor866@gmail.com>
2022-02-08 00:27:07 +0600

    added

--- client/src/Pages/Shared/Footer/Footer.js
+++ client/src/Pages/Shared/Footer/Footer.js
@@ -1,11 +1,12 @@
-import React from 'react';
+import React from "react";
 
 const Footer = () => {
-    return (
-        <div>
-            <h1>footer</h1>
-        </div>
-    );
+  return (
+    <div>
+      <h1>footer</h1>
+      <h1>This is footer</h1>
+    </div>
+  );
 };
 
 export default Footer;
