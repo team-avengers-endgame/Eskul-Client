40bf797f5713826c619052d1a1cb2509e4cc16ce
Maria Kamal <mariaafahad@gmail.com>
2022-02-08 20:19:55 +0600

    test

--- client/src/Pages/Home/Banner/Banner.js
+++ client/src/Pages/Home/Banner/Banner.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Banner = () => {
     return (
         <div>
-            <h1>Banner</h1>
+            <h1>Banner mine</h1>
             <h1>is here</h1>
         </div>
     );
