983d281008d9baee1f9ab7f520c2a00ce7463995
Farhan Elias <faarhaan10@gmail.com>
2022-02-09 01:59:26 +0600

    2nd issue

--- client/src/Pages/Shared/Navigation/Navigation.js
+++ client/src/Pages/Shared/Navigation/Navigation.js
@@ -5,7 +5,7 @@ const Navigation = () => {
     <>
 
       <nav class="#f8fafc">
-        <div className="max-w-7xl mx-auto px-3 sm:px-6 lg:px-8 mt-5">
+        <div className="max-w-7xl  mx-auto px-3 sm:px-6 lg:px-8 mt-5">
           <div class="relative flex items-center justify-between h-16">
             <div class="absolute inset-y-0 left-0 flex items-center sm:hidden">
               {/* <!-- Mobile menu button--> */}
