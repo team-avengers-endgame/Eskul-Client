fecfdc68834fb04b2eeb25e2eedbca74d5845b34
Farhan Elias <faarhaan10@gmail.com>
2022-02-09 18:54:12 +0600

    newsletter del

--- client/src/Pages/Home/Home.js
+++ client/src/Pages/Home/Home.js
@@ -3,7 +3,6 @@ import Footer from '../Shared/Footer/Footer';
 import Navigation from '../Shared/Navigation/Navigation';
 import Banner from './Banner/Banner';
 import Contact from './Contact/Contact';
-import Newsletter from './Newsletter/Newsletter';
 import Subscribe from './Subscribe/Subscribe';
 
 const Home = () => {
@@ -12,7 +11,6 @@ const Home = () => {
             <Navigation></Navigation>
             <Banner></Banner>
             <Contact />
-            <Newsletter />
             <Subscribe></Subscribe>
             <Footer></Footer>
         </div>
