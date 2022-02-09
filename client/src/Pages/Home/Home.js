21bc90472fe20f391592ac18e57f908d27bfac16
Maria Kamal <mariaafahad@gmail.com>
2022-02-09 18:07:15 +0600

    subscribe section

--- client/src/Pages/Home/Home.js
+++ client/src/Pages/Home/Home.js
@@ -2,13 +2,15 @@ import React from 'react';
 import Footer from '../Shared/Footer/Footer';
 import Navigation from '../Shared/Navigation/Navigation';
 import Banner from './Banner/Banner';
+import Subscribe from './Subscribe/Subscribe';
 
 const Home = () => {
     return (
         <div>
             <Navigation></Navigation>
-           <Banner></Banner>   
-           <Footer></Footer>
+            <Banner></Banner>
+            <Subscribe></Subscribe>
+            <Footer></Footer>
         </div>
     );
 };
