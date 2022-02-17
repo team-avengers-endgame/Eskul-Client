4a6bb03b460956d9af42bb27c72d22d68d794b51
Farhan Elias <faarhaan10@gmail.com>
2022-02-18 01:57:20 +0600

    banner added

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,17 @@
+import { useEffect } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from './Components/Home/Home/Home';
-
+import AOS from 'aos';
 
 function App() {
+  useEffect(() => {
+    AOS.init({
+      offset: 100,
+      duration: 500,
+      easing: 'ease'
+    });
+  });
+
   return (
     <BrowserRouter>
       <Routes>
