3939d5f461849aae972a551fee641e1b5f8fcdc9
Your Name <you@example.com>
2022-02-21 14:42:14 +0600

    school data from

---
+++ client/src/Components/Dashboard/SchoolDataPostFrome/SchoolDataPostFrome.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const SchoolDataPostFrom = () => {
+    return (
+        <div>
+            <h1>School data from</h1>
+        </div>
+    );
+};
+
+export default SchoolDataPostFrom;
