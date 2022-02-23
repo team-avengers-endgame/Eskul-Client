706125782145758644920aae97b6c1acb5cee40e
Your Name <you@example.com>
2022-02-23 23:10:28 +0600

    added a student file

---
+++ client/src/Components/Dashboard/Schools/AddedSchoolDetailsForm/AddAStudent/AddAStudent.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const AddAStudent = () => {
+    return (
+        <div>
+            <h1>Add a Student form</h1>
+        </div>
+    );
+};
+
+export default AddAStudent;
