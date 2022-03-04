f415027bdfe4c63739aba2a9d91f62833e59d78c
Your Name <you@example.com>
2022-03-04 11:21:51 +0600

    api change

--- client/src/Hooks/Api.js
+++ client/src/Hooks/Api.js
@@ -1,4 +1,4 @@
-const api1 = `http://localhost:8000/api`;
-// const api1 = `https://eskul-team-avengers.herokuapp.com/api`;
+// const api1 = `http://localhost:8000/api`;
+const api1 = `https://eskul-team-avengers.herokuapp.com/api`;
 export const api = api1;
 // http://localhost:8000/api/schools?page=2&limit=4
