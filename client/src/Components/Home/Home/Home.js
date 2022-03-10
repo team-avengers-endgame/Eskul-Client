5cc184cbb3a5151278d6f5dfd58bf65cee0bb1ea
niloy-koiry <niloy.koiry@node.inc>
2022-03-10 15:13:50 +0600

    Subscribe & Faq section created

--- client/src/Components/Home/Home/Home.js
+++ client/src/Components/Home/Home/Home.js
@@ -1,34 +1,38 @@
-import React from 'react';
-import Covid from '../../Dashboard/Covid/Covid';
-import Footer from '../../Shared/Footer/Footer';
-import NavigationBar from '../../Shared/NavigationBar/NavigationBar';
-import Banner from '../Banner/Banner';
-import Features from '../Features/Features';
-import Gallery from '../Gallery/Gallery';
-import NurtingEnvironment from '../NurtingEnvironment/NurtingEnvironment';
-import Partners from '../Partners/Partners';
-import QuickScroll from '../QuickScroll/QuickScroll';
-import Testimonial from '../Testimonial/Testimonial';
-import TopList from '../TopList/TopList';
-import WhyChooseUs from '../WhyChooseUs/WhyChooseUs';
+import React from "react";
+import Covid from "../../Dashboard/Covid/Covid";
+import Footer from "../../Shared/Footer/Footer";
+import NavigationBar from "../../Shared/NavigationBar/NavigationBar";
+import Subscribe from "../../Subscribe/Subscribe";
+import Faq from "../../Faq/Faq";
+import Banner from "../Banner/Banner";
+import Features from "../Features/Features";
+import Gallery from "../Gallery/Gallery";
+import NurtingEnvironment from "../NurtingEnvironment/NurtingEnvironment";
+import Partners from "../Partners/Partners";
+import QuickScroll from "../QuickScroll/QuickScroll";
+import Testimonial from "../Testimonial/Testimonial";
+import TopList from "../TopList/TopList";
+import WhyChooseUs from "../WhyChooseUs/WhyChooseUs";
 
 const Home = () => {
-    return (
-        <>
-            <NavigationBar />
-            <Banner />
-            <Covid />
-            <NurtingEnvironment />
-            <WhyChooseUs />
-            <Features />
-            <TopList />
-            <Testimonial />
-            <Gallery />
-            <Partners />
-            <QuickScroll />
-            <Footer />
-        </>
-    );
+  return (
+    <>
+      <NavigationBar />
+      <Banner />
+      <Covid />
+      <NurtingEnvironment />
+      <WhyChooseUs />
+      <Features />
+      <TopList />
+      <Testimonial />
+      <Gallery />
+      <Partners />
+      <Faq />
+      <QuickScroll />
+      <Subscribe />
+      <Footer />
+    </>
+  );
 };
 
 export default Home;
--- client/src/Components/Home/TopList/TopList.js
+++ client/src/Components/Home/TopList/TopList.js
@@ -1,62 +1,108 @@
-import * as React from 'react';
-import Table from '@mui/material/Table';
-import TableBody from '@mui/material/TableBody';
-import TableCell from '@mui/material/TableCell';
-import TableContainer from '@mui/material/TableContainer';
-import TableHead from '@mui/material/TableHead';
-import TableRow from '@mui/material/TableRow';
-import Paper from '@mui/material/Paper';
-import { Container, Typography } from '@mui/material';
+import * as React from "react";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import Paper from "@mui/material/Paper";
+import { Container, Typography } from "@mui/material";
 
 function createData(number, name, totalGPA) {
-    return { number, name, totalGPA };
+  return { number, name, totalGPA };
 }
 
 const rows = [
-    createData(1, 'Viqarunnisa Noon School And College', 1928),
-    createData(2, 'Government Labratory High School', 183),
-    createData(3, 'Ideal School & Colege, Dhaka', 1965),
-    createData(4, 'Rajuk Uttara Model College', 262),
-    createData(5, 'Dhaka Residential Model College, Dhaka', 262),
-
+  createData(1, "Viqarunnisa Noon School And College", 1928),
+  createData(2, "Government Labratory High School", 183),
+  createData(3, "Ideal School & Colege, Dhaka", 1965),
+  createData(4, "Rajuk Uttara Model College", 262),
+  createData(5, "Dhaka Residential Model College, Dhaka", 262),
 ];
 
 const TopList = () => {
-    return (
-
-
-        <Container>
-
-            <Typography sx={{ color: '#46AADC', pb: 2, fontFamily: 'Georgia', fontStyle: 'italic', fontWeight: '500', textAlign: 'center', margin: '5px' }} variant='h4'>Top Schools</Typography>
-            <TableContainer component={Paper}>
-                <Table aria-label="simple table">
-                    <TableHead sx={{ border: '1px solid #ccc', p: '8px', lineHeight: '1.5', verticalAlign: 'top', fontWeight: 'bold', textAlign: 'center', fontSize: '1.5rem', borderCollapse: 'collapse', gap: '10px' }}>
-                        <TableRow >
-                            <TableCell sx={{ textAlign: 'center' }}>Number</TableCell>
-                            <TableCell sx={{ textAlign: 'center' }} align="right">Name</TableCell>
-                            <TableCell sx={{ textAlign: 'center' }} align="right">Total GPA-5</TableCell>
-                        </TableRow>
-                    </TableHead>
-                    <TableBody>
-                        {rows.map((row) => (
-                            <TableRow
-                                key={row.name}
-                                sx={{ border: '1px solid #ccc', p: '8px', lineHeight: '1.5', verticalAlign: 'top', }}
-                            >
-                                <TableCell sx={{ textAlign: 'center' }} component="th" scope="row">
-                                    {row.number}
-                                </TableCell>
-                                <TableCell sx={{ textAlign: 'center' }} component="th" scope="row" align="right">{row.name}</TableCell>
-                                <TableCell sx={{ textAlign: 'center' }} component="th" scope="row" align="right">{row.totalGPA}</TableCell>
-
-                            </TableRow>
-                        ))}
-                    </TableBody>
-                </Table>
-            </TableContainer>
-        </Container>
-
-    );
+  return (
+    <Container>
+      <Typography
+        sx={{
+          color: "#46AADC",
+          pb: 2,
+          fontFamily: "Georgia",
+          fontStyle: "italic",
+          fontWeight: "500",
+          textAlign: "center",
+          margin: "5px",
+        }}
+        variant="h4"
+      >
+        Top Schools
+      </Typography>
+      <TableContainer component={Paper}>
+        <Table aria-label="simple table">
+          <TableHead
+            sx={{
+              border: "1px solid #ccc",
+              p: "8px",
+              lineHeight: "1.5",
+              verticalAlign: "top",
+              fontWeight: "bold",
+              textAlign: "center",
+              fontSize: "1.5rem",
+              borderCollapse: "collapse",
+              gap: "10px",
+            }}
+          >
+            <TableRow>
+              <TableCell sx={{ textAlign: "center" }}>Number</TableCell>
+              <TableCell sx={{ textAlign: "center" }} align="right">
+                Name
+              </TableCell>
+              <TableCell sx={{ textAlign: "center" }} align="right">
+                Total GPA-5
+              </TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {rows.map((row) => (
+              <TableRow
+                key={row.name}
+                sx={{
+                  border: "1px solid #ccc",
+                  p: "8px",
+                  lineHeight: "1.5",
+                  verticalAlign: "top",
+                }}
+              >
+                <TableCell
+                  sx={{ textAlign: "center" }}
+                  component="th"
+                  scope="row"
+                >
+                  {row.number}
+                </TableCell>
+                <TableCell
+                  sx={{ textAlign: "center" }}
+                  component="th"
+                  scope="row"
+                  align="right"
+                >
+                  {row.name}
+                </TableCell>
+                <TableCell
+                  sx={{ textAlign: "center" }}
+                  component="th"
+                  scope="row"
+                  align="right"
+                >
+                  {row.totalGPA}
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </Container>
+  );
 };
 
 export default TopList;
