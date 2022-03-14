9420687bafdf7858223bfa15aa23555051291e31
Maria Kamal <mariaafahad@gmail.com>
2022-03-15 01:19:18 +0600

    form

--- src/Hooks/Api.js
+++ src/Hooks/Api.js
@@ -1,6 +1,4 @@
 // const api1 = `http://localhost:8000/api`;
-const api1 = `https://radiant-thicket-48216.herokuapp.com/api`;
+
+const api1 = `https://eskul-server.herokuapp.com/api`;
 export const api = api1;
-// http://localhost:8000/api/schools?page=2&limit=4cd
-// 2 https://radiant-thicket-48216.herokuapp.com/api
-// 1 https://eskul-team-avengers.herokuapp.com/api
